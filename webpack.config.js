1cf07049fc1e23457d38c146c7f9d359acf15488
Patria <patria2826@gmail.com>
2019-09-24 18:23:31 +0800

    style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,17 @@ const path = require("path");
 module.exports = {
     mode: "development", //setting mode to "development" can make main.js readable to human
     // devtool: "none", //setting devtool to "none" can remove the "eval" in main.js 
-    entry: "./src/index.js",
+    entry: "./src/index.js", //grab everything related to this file
     output: {
         filename: "main.js", //the file name can be whatever you want it to be
         path: path.resolve(__dirname, "dist"), //the file name can be whatever you want it to be
     },
-}
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"] //load in reverse order, so we put style in front of css
+            }
+        ]
+    }
+};
